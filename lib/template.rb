37a09ad3ced7a5c0c0f6fba9a64af67031f2f659
Flurin Egger <flurin@digitpaint.nl>
2009-04-02 12:08:23 +0200

    Fix the template class to use the parser correctly.

--- lib/template.rb
+++ lib/template.rb
@@ -4,7 +4,8 @@ module Personify
     attr_reader :template
     
     def initialize(template)
-      @template = PersonifyLanguageParser.parse(template)
+      parser = PersonifyLanguageParser.new
+      @template = parser.parse(template)
     end
     
     def render(local_assigns={})
